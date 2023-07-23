09a6c3070063a01575a59dc3e0857db14d4cf84a
Kelvin Dias <111926527+Kelvinbd@users.noreply.github.com>
2023-07-23 20:33:44 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,22 @@
+// function submeter() {
+//     console.log('form click');
+
+//     let nome = document.getElementById('nome').value;
+//     let email = document.getElementById('email').value;
+//     let mensagem = document.getElementById('mensagem').value;
+
+//     console.log('nome form: ' + nome);
+//     console.log('email form: ' + email);
+//     console.log('mensagem form: ' + mensagem);
+//     }
+function submeter() {
+
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let mensagem = document.getElementById('mensagem').value;
+
+    console.log('nome form: ' + nome);
+    console.log('email form: ' + email);
+    console.log('mensagem form: ' + mensagem);
+    }
+   
